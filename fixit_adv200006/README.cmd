98a6fce4b3e5e5fd2489ed5183f857a06abddc0c
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2020-03-25 16:22:26 +0900

    Create README.cmd

---
+++ fixit_adv200006/README.cmd
@@ -0,0 +1,4 @@
+Fix it for ADV200006 Vulnerability
+ADV200006 | Type 1 Font Parsing Remote Code Execution Vulnerability
+https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006
+
