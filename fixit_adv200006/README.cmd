786605c92561a2c9fc1344b5f2bbb60e5bf1b01a
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2020-03-26 14:50:31 +0900

    Update README.cmd

--- fixit_adv200006/README.cmd
+++ fixit_adv200006/README.cmd
@@ -3,6 +3,4 @@ Fix it for ADV200006 Vulnerability
 ADV200006 | Type 1 Font Parsing Remote Code Execution Vulnerability
 https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006
 
-<s>Windows 8.1/Server 2012 R2 以前の場合は、回避策適用バッチ（fixit_adv200006.cmd）を 2 回実行すると確実。
-そうしないと DisableATMFD レジストリがちゃんと設定されないことがあるみたい。回避策解除のための Undo バッチ（undofixit_...）も同様。</s>
-2020/3/26 JST fix bug
+2020/3/26 JST fixed bug
