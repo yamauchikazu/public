3ba2fba3d8ac2e6e271b24fe82b85a8c7b9a8aa1
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2020-03-28 07:20:10 +0900

    Update README.cmd

--- fixit_adv200006/README.cmd
+++ fixit_adv200006/README.cmd
@@ -3,4 +3,5 @@ Fix it for ADV200006 Vulnerability
 ADV200006 | Type 1 Font Parsing Remote Code Execution Vulnerability
 https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006
 
-2020/3/26 JST fixed bug
+2020/03/26 JST fixed bug
+2020/03/28 JST fixed code according to the security advisory ver 1.5.
