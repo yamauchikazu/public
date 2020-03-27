9f33b8945d10e7f3be8efb5d23b35473af3ae5a6
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2020-03-28 07:17:36 +0900

    Update fixit_adv20006.cmd

--- fixit_adv200006/fixit_adv20006.cmd
+++ fixit_adv200006/fixit_adv20006.cmd
@@ -37,11 +37,11 @@ rename atmfd.dll x-atmfd.dll
 popd
 
 :SKIPRENAME
-REM WMIC OS GET VERSION | find "10." > nul
-REM IF not errorlevel 1 GOTO WINDOWS_10
+WMIC OS GET VERSION | find "10." > nul
+IF not errorlevel 1 GOTO WINDOWS_10
 
 REM ECHO [3/3+] Optional procedure for Windows 8.1 operating systems and below (disable ATMFD)
-ECHO [3/3+] Optional procedure for Windows 7 to 10 ver 1703 (disable ATMFD)
+ECHO [3/3+] Optional procedure for Windows 7 to 8.1 and Server 2008 to 2012 R2 (disable ATMFD)
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v DisableATMFD /t REG_DWORD /d 0 /f
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v DisableATMFD /t REG_DWORD /d 1 /f
 REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v DisableATMFD
