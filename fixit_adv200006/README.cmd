c756e13144e923f6194ed6608cd65aea1641d4b2
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2020-03-31 10:59:42 +0900

    Update README.cmd

--- fixit_adv200006/README.cmd
+++ fixit_adv200006/README.cmd
@@ -1,14 +1,14 @@
 Fix it for ADV200006 Vulnerability
 
 ADV200006 | Type 1 Font Parsing Remote Code Execution Vulnerability
-https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006
+<a href="https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006">https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200006</a>
 
 2020/03/26 JST fixed bug
 2020/03/28 JST fixed code according to the security advisory ver 1.5.
 
 ADV200001 | Microsoft Guidance on Scripting Engine Memory Corruption Vulnerability（Security Advisory）
-https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/ADV200001
+<a href="https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/adv200001">https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/ADV200001</a>
 
 Reference
 緊急、Windowsの未パッチの脆弱性（ADV200006）回避策をバッチ化する（@IT）
-https://www.atmarkit.co.jp/ait/articles/2003/30/news028.html
+<a href="https://www.atmarkit.co.jp/ait/articles/2003/30/news028.html">https://www.atmarkit.co.jp/ait/articles/2003/30/news028.html</a>
