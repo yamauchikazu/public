fe8d0e36e2fdc75db5cae45c5db90e589759706f
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2021-07-03 12:39:47 +0900

    Delete testPsScript.ps1

--- aibuilder/testPsScript.ps1
+++
@@ -1,3 +0,0 @@
-mkdir c:\buildArtifacts
-echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\azureImageBuilder.txt
-curl https://github.com/yamauchikazu/public/raw/master/aibuilder/ja-jp.cab -o c:\buildArtifacts\Microsoft-Windows-Server-Language-Pack_x64_ja-jp.cab
