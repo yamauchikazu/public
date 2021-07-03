0577a86eb66649e7849ec2a7a25869357406a3b0
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2021-07-03 09:13:46 +0900

    Update testPsScript.ps1

--- aibuilder/testPsScript.ps1
+++ aibuilder/testPsScript.ps1
@@ -1,3 +1,3 @@
 mkdir c:\buildArtifacts
 echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\azureImageBuilder.txt
-curl https://github.com/yamauchikazu/public/blob/master/aibuilder/ja-jp.cab -o c:\buildArtifacts\Microsoft-Windows-Server-Language-Pack_x64_ja-jp.cab
+curl https://github.com/yamauchikazu/public/raw/master/aibuilder/ja-jp.cab -o c:\buildArtifacts\Microsoft-Windows-Server-Language-Pack_x64_ja-jp.cab
