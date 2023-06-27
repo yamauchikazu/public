2b140a6f2b798178aa57775f3eac0a64a1685ab9
yamaryo <30482510+yamauchikazu@users.noreply.github.com>
2023-06-27 17:42:01 +0900

    Add files via upload

--- get-m365ver2.ps1
+++ get-m365ver2.ps1
@@ -5,19 +5,24 @@ if ($O365CurrentVer.Length -eq 0) {
     Write-Host "Microsoft 365 Apps (C2R) is not installed on this PC."
     exit
 } else {
-  if ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration").Platform -eq "x64") {
+  $O365Platform = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration").Platform
+  if ($O365Platform -eq "x64") {
     $O365Bitness = "64-bit" 
   } else {
     $O365Bitness = "32-bit" 
   }
 }
-$O365VerStr = "unknown"
 $O365BuildStr = "(Build "+$O365CurrentVer.Remove(1,5)+")"
+$O365VerStr = "unknown"
 $OfficeVerHisURL = "https://learn.microsoft.com/en-us/officeupdates/update-history-microsoft365-apps-by-date"
-$webcontent = (Invoke-WebRequest -uri $OfficeVerHisURL).Content
-if ($webcontent.contains($O365BuildStr)) { 
-  if ($webcontent.Substring($webcontent.IndexOf($O365BuildStr)-6,6).Trim() -match '[0-9]{4}'){
-    $O365VerStr = $webcontent.Substring($webcontent.IndexOf($O365BuildStr)-6,6).Trim()
+try {
+  $webcontent = (Invoke-WebRequest -uri $OfficeVerHisURL).Content
+  if ($webcontent.contains($O365BuildStr)) { 
+    if ($webcontent.Substring($webcontent.IndexOf($O365BuildStr)-6,6).Trim() -match '[0-9]{4}'){
+      $O365VerStr = $webcontent.Substring($webcontent.IndexOf($O365BuildStr)-6,6).Trim()
+    }
   }
+} catch {
+  Write-Host "Error: Failed to get the release information from Microsoft Web Site."
 }
 Write-Host "Version" $O365VerStr $O365BuildStr $O365Bitness
